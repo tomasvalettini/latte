e168fc877a99696398344b8e051fe18e3023ff87
Tomas Valettini <tomasvalettini@users.noreply.github.com>
2026-02-22 19:51:57 -0500

    updated commands short/long descriptions (#7)

--- cmd/update.go
+++ cmd/update.go
@@ -14,13 +14,8 @@ import (
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
 	Use:   "update",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "update task",
+	Long:  `Command to update task with specific id.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		id, err := strconv.Atoi(args[0])
 		if err != nil {
